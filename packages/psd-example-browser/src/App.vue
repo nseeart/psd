18bd7bb69a7c0a2b018e2db7396a8578e9e105ec
n.see <gooarter@gmail.com>
2022-12-20 22:24:05 +0800

    feat(psd-example-browser): 优化demo

--- packages/psd-example-browser/src/App.vue
+++ packages/psd-example-browser/src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <div id="dropzone">
+        <div ref="dropRef" class="dropzone">
             <p>Drop here</p>
         </div>
         <div id="content">
@@ -16,23 +16,43 @@ import PSD from "@n.see/psd-parser";
 
 const dataRef = ref();
 const imageRef = ref();
+const dropRef = ref();
+
+const onDragOver = (event: any) => {
+    event.stopPropagation();
+    event.preventDefault();
+    event.dataTransfer.dropEffect = "copy";
+};
+
+const onDrop = (evt: Event) => {
+    evt.stopPropagation();
+    evt.preventDefault();
+    PSD.fromEvent(evt).then((psd: PSD) => {
+        view(psd);
+    });
+};
+
+function view(psd: PSD) {
+    const data = psd.tree().export();
+    const dataString = JSON.stringify(data, undefined, 2);
+    if (dataRef.value) {
+        dataRef.value.innerHTML = dataString;
+    }
+    const img = (psd.image as any).toPng();
+    if (imageRef.value) {
+        imageRef.value.appendChild(img);
+    }
+
+    console.log("psd data:", data);
+}
 
 onMounted(() => {
+    dropRef.value.addEventListener("dragover", onDragOver, true);
+    dropRef.value.addEventListener("drop", onDrop, true);
     PSD.fromURL("/test2.psd").then((psd: PSD) => {
-        console.log("psd==========", psd);
-        const data = psd.tree().export();
-        const dataString = JSON.stringify(data, undefined, 2);
-        if (dataRef.value) {
-            dataRef.value.innerHTML = dataString;
-        }
-        const img = (psd.image as any).toPng();
-        if (imageRef.value) {
-            imageRef.value.appendChild(img);
-        }
-
-        const node = psd.tree();
-        console.log("node", node);
+        view(psd);
     });
+    dropRef.value;
 });
 </script>
 <style lang="scss" scoped>
@@ -45,16 +65,17 @@ onMounted(() => {
     padding: 10px;
     margin: 0;
     flex: 1;
+    overflow: hidden;
 }
 
-#dropzone {
+.dropzone {
     width: 500px;
     height: 100px;
     border: 1px #ababab dashed;
     margin: 50px auto;
 }
 
-#dropzone p {
+.dropzone p {
     text-align: center;
     line-height: 100px;
     margin: 0;
